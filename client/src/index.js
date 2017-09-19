1e8d5f9cedefc03c3366d88e0747047cb4055ea6
Lenora Rigoni <lenorarigoni@gmail.com>
2017-09-19 15:08:54 -0400

    install bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './router';
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 ReactDOM.render(Router, document.getElementById('root'));
