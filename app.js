29c86e062f15cf651c64107a55f1feb9f5d92039
Michael Gutleber <michaeljgut@gmail.com>
2017-09-19 10:10:56 -0400

    added app.js, package files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('client/build'))
+
+
+// app.get('/', function(req, res) {
+//     res.render('hello world');
+// });
+
+app.listen(3001, function(){
+    console.log('Example app listening on port 3000!')    
+})
+
+
