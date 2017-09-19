d96dd3b08c11cf5840d8b2ea0135bbb0b4875417
Lenora Rigoni <lenorarigoni@gmail.com>
2017-09-19 12:10:17 -0400

    import flexbox-react

---
+++ client/src/components/Header.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import App from '../App';
+import Flexbox from 'flexbox-react';
+
+
+
+class Header extends Component {
+
+    handleClickNav(){
+        console.log('dropdown nav bar https://www.npmjs.com/package/react-sliding-pane')
+    }
+
+    render(){
+        return(
+            <div className='header'>
+                <div className='hamburger' onClick={() => this.handleClickNav()}>
+                    <div className="hamburger-nav-icon"></div>
+                    <div className="hamburger-nav-icon"></div>
+                    <div className="hamburger-nav-icon"></div>
+                </div>
+                
+                <h1 className='website-title-header'><Link to='/'>Medi-flash</Link></h1>
+                <h4 className='user-name-header'>username</h4>
+            </div>
+        )
+    }
+
+}
+
+export default Header;
