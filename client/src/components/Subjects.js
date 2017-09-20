aa89f6aa39de506fc711dc28cbe527e0bffe5268
Lenora Rigoni <lenorarigoni@gmail.com>
2017-09-20 19:11:05 -0400

    target flashcardtoggle

--- client/src/components/Subjects.js
+++ client/src/components/Subjects.js
@@ -1,36 +1,67 @@
-// import React, {Component} from 'react';
-// import { Link } from 'react-router-dom';
-// import Subject from './Subject';
+import React, {Component} from 'react';
+
+import { Link } from 'react-router-dom';
+
+import axios from 'axios';
+
+import Subject from './Subject';
 
 
-// class Subjects extends Component {
-//     constructor(){
-//       super();
-//       this.state = {
-//         subjects: subjects
-//       }
-//     }
+class Subjects extends Component {
+    constructor(){
+      super();
+      this.state = {
+        subjects: [], 
+        subjectsLoaded: false, 
+      }
+      this.handleClick = this.handleClick.bind(this)
+    }
   
-//     renderSubjects(array){
-//       return array.map(subject => {
-//         return (
-//           <li key={subject.id} className='individual-subject'>
-//             <Link to={`/subjects/${subject.id}`}>{subject.name} </Link>
-//           </li>
-//         )
-//       })
-//     }
+
+    componentDidMount() {
+        axios.get('/subjects')
+          .then(res => {
+            this.setState({
+                subjects: res.data.subjects, 
+                //console.log data make sure I am targetting the correct thing 
+                subjectsLoaded: true
+                
+            })
+
+          })
+          
+          console.log('------->this is the state from subjects',this.state)
+      }
+
+
+    renderSubjects(array){
+       if(this.state.subjectsLoaded) {
+            return array.map(subject => {
+                return (
+                <li key={subject.id} className='individual-subject'>
+                    <Link to={`/subjects/${subject.id}`}>{subject.name}</Link>
+                </li>
+                )
+            })
+       } else {
+           return <p>no subjects yet!</p>
+       }
+
+    }
+
+
   
-//     render(){
-//       return(
-//         <div className='subjects'>
-//           <ul className='list-of-subjects'>
-//             {this.renderSubjects(this.state.subjects)}
-//           </ul>
-//         </div>
-//       )
-//     }
-//   }
+    render(){
+      return(
+        <div className='subjects'>
+          <h1 className='subject-page-header'>Subjects</h1>
+          <ul className='list-of-subjects'>
+            {this.renderSubjects(this.state.subjects)}
+          </ul>
+        </div>
+      )
+    }
+  }
   
-//   export default Subjects;
+  export default Subjects;
   
--- client/src/router.js
+++ client/src/router.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom';
-// import Subjects from './components/Subjects'
+import Subjects from './components/Subjects'
 // import Subject from './components/Subject'
 
 import Login from './components/Login.js'
@@ -16,7 +16,10 @@ export default (
         <div className='router'>
             <Header />
             <Route exact path='/home' component={App} />
-            {/* <Route exact path='/subjects' component={Subjects} /> */}
+            <Route exact path='/subjects' component={Subjects} />
+            <Route exact path='/add' component={flashcardAdd} />
+
+
             {/* <Route exact path='/Subject:id' component={Subject} /> */}
             <Route exact path='/' component={Login} />
             <Route exact path='/register' component={Register} />
