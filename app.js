a68078b7cec836800689f13476ef3c21611b3757
Michael Gutleber <michaeljgut@gmail.com>
2017-09-21 09:58:30 -0400

    Changed backend to 3000, frontend to 3001

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.session());
      res.send('hello world');
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
