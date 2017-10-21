cbb20bc3a5d6f3b1a1f7d8af5e9935cd45a7586e
Michael Gutleber <michaeljgut@gmail.com>
2017-10-21 18:46:16 -0400

    Allow edit of card term

--- client/src/components/FlashcardEditForm.js
+++ client/src/components/FlashcardEditForm.js
@@ -35,10 +35,11 @@ class FlashcardEditForm extends Component {
     e.preventDefault();
     console.log('in handleInputChange');
     console.log(e.target.value);
-    // const name = e.target.name;
-    // const value = e.target.value;
+    const name = e.target.name;
+    const value = e.target.value;
     this.setState({
-      definition: e.target.value,
+      [name]: value
+      // definition: e.target.value,
     });
   }
 
